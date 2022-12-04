eff5c3de0255eae94b6e3ab3c6ceba18d694792d
jhon0608 <macleanjhon8@gmail.com>
2022-12-04 00:45:06 -0500

    primer paso

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,44 @@
+
+import Paciente from "./Paciente";
+
+const ListadoPacientes = ({ pacientes, setPaciente }) => {
+   
+
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+
+      {pacientes && pacientes.length ?(
+        <>
+            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Administra tus {""}
+              <span className="text-indigo-600 font-bold text-xlS">Pacientes y Citas </span>
+            </p>
+            {pacientes.map(paciente => (
+              <Paciente 
+                   key={paciente.id}
+                   paciente={paciente} 
+                   setPaciente={setPaciente}
+              />
+            ))}
+            </>
+              
+
+      ) : (
+        <>
+               <h2 className="font-black text-3xl text-center">no hay Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+                 Comienza agregando pacientes {''}
+              <span className="text-indigo-600 font-bold text-xlS">y apareceran en este lugar </span>
+            </p>
+
+
+        </>
+      )}
+
+ 
+    </div>
+  );
+};
+
+export default ListadoPacientes;
